4a8b01fa248bd28f17f6d493219d2bc25d8c0630
spedaco6 <spedaco6@gmail.com>
2025-08-27 17:03:33 -0700

    Updated LoginForm styling

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -4,7 +4,7 @@ import Form from "./Form";
 import Input from "../Input";
 
 export default function LoginForm({ className="" }: React.PropsWithChildren<{ className?: string }>): React.ReactElement {
-  return <Form className={`w-1/4 m-2 border-2 p-4 rounded-xl ${className}`}>
+  return <Form className={`sm:w-[25rem] w-4/5 m-2 border-2 p-4 rounded-xl ${className}`}>
     <h2 className="text-2xl">Login</h2>
     <Input title="Email" autoFocus />
     <Input title="Password" />
