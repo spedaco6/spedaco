fc1a38f82d970e9f3312e337cf5f343912934e70
spedaco6 <spedaco6@gmail.com>
2025-09-27 22:26:25 -0400

    Add formAction to EmailForm

--- components/Forms/EmailForm.tsx
+++ components/Forms/EmailForm.tsx
@@ -1,17 +1,19 @@
 "use client";
-import React from "react";
+import React, { useActionState } from "react";
 import Input from "../Input";
 import useInput, { UseInputReturn } from "@/hooks/useInput";
 import { Validator } from "@/lib/Validator";
+import { forgotPassword } from "@/app/forgot-password/actions";
 
 const IS_EMAIL = [Validator.isEmail];
 
 export default function EmailForm(): React.ReactElement {
   const email: UseInputReturn = useInput("email*", "", IS_EMAIL);
   
-
-
-  return <form className="border-1 border-gray rounded-sm w-100 p-4">
+  const [{ success },formAction] = useActionState(forgotPassword, { success: false });
+  console.log(success);
+  
+  return <form className="border-1 border-gray rounded-sm w-100 p-4" action={formAction}>
     <Input title="Enter your email" type="email" hook={email} />
     <div className="flex justify-end mt-4"><button>Submit</button></div>
   </form>
