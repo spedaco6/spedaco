0812d38bd292aa474682b1d65a761cc10bd9f688
spedaco6 <spedaco6@gmail.com>
2025-09-30 14:15:32 -0400

    Add comments to auth files

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,9 +2,13 @@ import { AuthActionResponse, authenticateUser } from "@/lib/auth";
 import { sanitize } from "@/lib/utils";
 
 export const login = async (prevValues: AuthActionResponse, formData: FormData): Promise<AuthActionResponse> => {
+  // Ensure formData exists
   if (!formData) return { success: false, error: "No data was provided" };
+  
+  // Sanitize data
   const sanitized = sanitize(formData);
   
+  // Complete authentication action
   let result: AuthActionResponse;
   try {
     result = await authenticateUser(String(sanitized.email), String(sanitized.password));
@@ -14,5 +18,9 @@ export const login = async (prevValues: AuthActionResponse, formData: FormData):
     const message = err instanceof Error ? err.message : "Something went wrong. Could not authenticate user";
     return { success: false, error: message, prevValues: { email: sanitized.email } };
   }
+
+  // Create cookie
+
+  // return result
   return { success: true, token: result.token, userId: result.userId };
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,18 @@ export interface AuthActionResponse {
 }
 
 export const authenticateUser = async (email: string, password: string): Promise<AuthActionResponse> => {
+  // Ensure data is provided
   console.log(email);
   console.log(password);
+
+  // Find user
+
+  // Validate password
+
+  // Create token
+
+  // Save token to database
+
+  // Return result
   return { success: true };
 }
