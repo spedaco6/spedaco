7421e42d87cc503a7c1bfcd61f1fff4792e35d53
Spencer Conrad <spencerconrad@Spencers-MacBook-Air.local>
2025-10-03 10:53:23 -0400

    Improve testing mocks for verify actions

--- app/verify/actions.test.js
+++ app/verify/actions.test.js
@@ -1,14 +1,36 @@
 import { describe, test, vi, expect } from "vitest";
 import * as accounts from "./actions";
-import * as tokens from "@/lib/tokens";
+import { User } from "@/models/User";
 import { verifyEmailVerificationToken } from "../../lib/tokens";
+import { verifyAccount } from "./actions";
+
+vi.mock("@/lib/tokens", () => ({
+  verifyEmailVerificationToken: vi.fn(() => ({ 
+    userId: "userId",
+    jti: "jti",
+    intent: "email_verification",
+  })),
+}));
+
+const mockSave = vi.fn(function () { return Promise.resolve(this) });
+vi.mock("@/models/User", () => {
+  const mockFindById = vi.fn(() => Promise.resolve({ 
+    _id: "userId", 
+    verificationToken: "emailVerificationToken", 
+    save: mockSave 
+  }));
+  const mockUser = vi.fn((info) => ({
+    ...info,
+    save: mockSave,
+  }));
+  mockUser.findById = mockFindById;
+  return { User: mockUser };
+});
+
+vi.spyOn(console, "error").mockImplementation(() => {});
 
 describe("verifyAccount", () => {
-  beforeAll(() => {
-    // Stifle testing error logs
-    vi.spyOn(console, "error").mockImplementation(() => {});
-  });
-  beforeEach(() => { vi.clearAllMocks() });
+  beforeEach(() => vi.clearAllMocks());
   afterAll(() => {
     vi.resetAllMocks();
     vi.resetModules();
@@ -16,77 +38,37 @@ describe("verifyAccount", () => {
   test("is defined", () => {
     expect(accounts.verifyAccount).toBeDefined();
   });
-  test.each([undefined, null, 123, true, false, ""])("returns false if token is not string", async (val) => {
+  test.each([undefined, null, 123, true, false, ""])("%s returns false if token is not string", async (val) => {
     const result = await accounts.verifyAccount(val);
     expect(result).toBe(false);
   });
   test("calls verifyEmailVerificationToken once", async () => {
-    const spy = vi.spyOn(tokens, "verifyEmailVerificationToken");
     await accounts.verifyAccount("token123");
-    expect(spy).toHaveBeenCalledOnce();
+    expect(verifyEmailVerificationToken).toHaveBeenCalledOnce();
   });
   test("returns false when verification token is not verified", async () => {
-    vi.doMock("@/lib/tokens", () => ({
-      verifyEmailVerificationToken: vi.fn(() => false),
-    }));
+    verifyEmailVerificationToken.mockImplementationOnce(() => ({ error: "Error message" }));
     const result = await accounts.verifyAccount("token123");
     expect(result).toBe(false);
   });
   test("returns false when no user is found with matching userId", async () => {
-    vi.resetModules();
-    vi.doMock("@/lib/tokens", () => ({
-      verifyEmailVerificationToken: vi.fn(() => ({ userId: "ABC123" })),
-    }));
-    vi.doMock("@/models/User", () => {
-      const mockSave = vi.fn(() => Promise.resolve(true));
-      const mockFindById = vi.fn(() => Promise.resolve(null));
-      const mockUser = vi.fn(() => ({ save: mockSave }));
-      mockUser.findById = mockFindById;
-      return { User: mockUser };
-    });
-    const { verifyAccount: verifyAccountNoUserFound } = await import("./actions");
-    const result = await verifyAccountNoUserFound("noUserFound");
+    User.findById.mockImplementationOnce(() => Promise.resolve(null));
+    const result = await verifyAccount("noUserFound");
+    expect(result).toBe(false);
+  });
+  test("returns false when User.findById throws error", async () => {
+    User.findById.mockImplementationOnce(() => Promise.reject(new Error("Error message")));
+    const result = await verifyAccount("noUserFound");
     expect(result).toBe(false);
   });
   test("returns false when verification token does not match user's verificationToken", async () => {
-    vi.resetModules();
-    vi.doMock("@/lib/tokens", () => ({
-      verifyEmailVerificationToken: vi.fn(() => ({ userId: "ABC123" })),
-    }));
-    vi.doMock("@/models/User", () => {
-      const mockSave = vi.fn(() => Promise.resolve(true));
-      const mockFindById = vi.fn(() => Promise.resolve({ _id: "ABC123", verificationToken: "token123"}));
-      const mockUser = vi.fn(() => ({ save: mockSave }));
-      mockUser.findById = mockFindById;
-      return { User: mockUser };
-    });
-    const { verifyAccount: verifyAccountWithMismatchedToken } = await import("./actions");
-    const result = await verifyAccountWithMismatchedToken("mismatch");
+    verifyEmailVerificationToken.mockImplementationOnce(() => ({ error: "Token mismatch"}))
+    const result = await verifyAccount("mismatch");
     expect(result).toBe(false);
   });
   test("returns verified user and deletes token when successfully completed", async () => {
-    vi.resetModules();
-    const mockSave = vi.fn(function () {
-      return Promise.resolve(this);
-    });
+    const result = await verifyAccount("emailVerificationToken");
     
-    vi.doMock("@/models/User", () => {
-      const mockFindById = vi.fn(() => Promise.resolve({ 
-        _id: "ABC123", 
-        verificationToken: "token123",
-        save: mockSave,
-      }));
-      const mockUser = vi.fn(() => ({ save: mockSave }));
-      mockUser.findById = mockFindById;
-      return { User: mockUser };
-    });
-    
-    vi.doMock("@/lib/tokens", () => ({
-      verifyEmailVerificationToken: vi.fn(() => ({ userId: "ABC123" })),
-    }));
-
-    const { verifyAccount: verifyAccountSuccess } = await import("./actions");
-    const result = await verifyAccountSuccess("token123");
     expect(result).toBe(true);
     expect(mockSave).toHaveBeenCalledOnce();
     
--- app/verify/actions.ts
+++ app/verify/actions.ts
@@ -1,10 +1,9 @@
 import { DecodedToken, verifyEmailVerificationToken } from "@/lib/tokens";
-import { User } from "@/models/User";
+import { IUser, User } from "@/models/User";
 
 export const verifyAccount = async (token: string): Promise<boolean> => {
   // Ensure token is defined and type string
   if (!token || typeof token !== "string") return false;
-  
   // Decode token
   let decoded: DecodedToken;
   try {
@@ -13,13 +12,19 @@ export const verifyAccount = async (token: string): Promise<boolean> => {
     console.error(err);
     return false;
   }
-
-  // If no token, retur false
+  
+  // If no token, return false
   if (decoded?.error) return false;
-  const user = await User.findById(decoded?.userId);
+  let user: IUser | null = null;
+  try {
+    user = await User.findById(decoded?.userId);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
   if (!user) return false;
   if (user?.verificationToken !== token) return false;
-
+  
   // Update and save user
   user.verified = true;
   user.verificationToken = undefined;
