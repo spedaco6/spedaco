24fb2452f30c20f28f661b2f53fd89c10ac395a9
Spencer Conrad <spencerconrad@Spencers-MacBook-Air.local>
2025-10-05 21:44:27 -0400

    add useEffects to manage access tokens

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,21 +1,40 @@
 "use client";
-import React from "react";
+import React, { useActionState, useEffect } from "react";
 import Input from "../Input";
 import useInput, { UseInputReturn } from "@/hooks/useInput";
 import { Validator } from "@/lib/Validator";
+import { login } from "@/app/login/actions";
 
 const IS_EMAIL = [Validator.isEmail];
 
 export default function LoginForm({ className="" }: React.PropsWithChildren<{ className?: string }>): React.ReactElement {
   const email: UseInputReturn = useInput("email*", "", IS_EMAIL, { message: "Email is required" });
   const password: UseInputReturn = useInput("password*", "", [], { message: "Password is required"});
+  const [ authState, loginAction, isPending ] = useActionState(login, { success: false });
+  
+  useEffect(() => {
+    if (authState.refreshToken) {
+      // Set session cookie here
+      console.log("refresh", authState.refreshToken);
+    }
+  }, [authState.refreshToken]);
 
-  return <form className={`sm:w-[25rem] w-4/5 m-2 border-2 p-4 rounded-xl ${className}`}>
+  useEffect(() => {
+    if (authState.accessToken) {
+      localStorage.setItem("accessToken", authState.accessToken);
+    }
+  }, [authState.accessToken]);
+
+  return <form 
+    className={`sm:w-[25rem] w-4/5 m-2 border-2 p-4 rounded-xl ${className}`} 
+    action={loginAction}
+  >
+    { authState.error && <p className="text-error">{authState.error}</p> }
     <h2 className="text-2xl">Login</h2>
-    <Input title="Email" hook={email} autoFocus />
-    <Input title="Password" type="password" hook={password} />
+    <Input title="Email" hook={email} autoFocus disabled={isPending} />
+    <Input title="Password" type="password" hook={password} disabled={isPending} />
     <div className="flex justify-end m-2 mt-4">
-      <button>Login</button>
+      <button disabled={isPending}>{isPending ? "Logging in..." : "Login"}</button>
     </div>
   </form>
 }
